43222ee8fba5ed2e391764f31276c743aa7f81ef
Lakhwinder Singh <lakhwinder.code@gmail.com>
2024-08-26 14:57:06 +0530

    added some files

---
+++ week2/patients.js
@@ -0,0 +1,13 @@
+const patients = [
+    {name: "Lakhwinder",
+     kidneys: [{healthy: false}, {healthy: true}]
+    },
+    {
+        name: "Ayushi",
+        kidneys: [{healthy: true}, {healthy:true}]
+    }
+];
+
+const name = "Lakhwinder Singh Sidhu"
+
+module.exports = {patients,name};
